18dfd6f43ee690c3a1078832ffcc02e8221a6681
nikhilsinghrathore1 <111992033+nikhilsinghrathore1@users.noreply.github.com>
2024-05-26 01:40:45 +0530

    rewiring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,25 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './components/Login'
-import Signup from './components/Signup'
-
-import {BrowserRouter,Route , Routes} from "react-router-dom"
-import Home from './components/Home'
+import Landing from './pages/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <BrowserRouter>
- 
-    <Routes>
-            <Route path="/" element={<Login/>}></Route>
-            <Route path="/register" element={<Signup/>}></Route>
-            <Route path='/home' element = {<Home/>}></Route>
-    </Routes>
-    </BrowserRouter>
+      <Landing/>
     </>
   )
 }
